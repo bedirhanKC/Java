cf9ade7c2d587425f57a33f36a691a1e1011d0e5
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-09-20 18:49:48 +0300

    Add files via upload

---
+++ Ogrenim_Kayitlari/ders2_degiskenler.java
@@ -0,0 +1,14 @@
+package intro;
+
+public class ders2_degiskenler {
+
+	public static void main(String[] args) {
+
+		int ogrenciSayisi = 42; // int bir değişken tipidir. burda bir değişken oluşturulur.
+		String mesaj = "ogrenci sayisi : "; // string bir char dizisidir. bir metin tutar.
+		
+		System.out.println(mesaj +ogrenciSayisi); 
+		
+	}
+
+}
