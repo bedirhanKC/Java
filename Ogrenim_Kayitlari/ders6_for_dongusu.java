d7682e82d2b6b0d0c10948537df555ec9bbb07b8
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-09-20 18:53:11 +0300

    Add files via upload

---
+++ Ogrenim_Kayitlari/ders6_for_dongusu.java
@@ -0,0 +1,19 @@
+package intro;
+
+public class ders6_for_dongusu {
+
+	public static void main(String[] args) {
+		
+		int i;
+		
+		for(i=1;i<=10;i++) { // for( i=1 başlangıç değeridir ; i<10 i 10dan küçük olduğu sürece devam et anlamına gelir ; i++ i'yi birer birer arttır)
+			
+			System.out.println(i);
+			
+		}
+		
+		System.out.println("döngü sonlandı.");
+
+	}
+
+}
