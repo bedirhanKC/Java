e43263ca3a0ec829440f3a4ff5321e53e74289f7
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-09-20 18:50:00 +0300

    Add files via upload

---
+++ Ogrenim_Kayitlari/ders3_veri_tipleri.java
@@ -0,0 +1,22 @@
+package intro;
+
+public class ders3_veri_tipleri {
+
+	public static void main(String[] args) {
+		
+		//değişkenler
+		
+		boolean dogruMu = true; // 0lar ve 1ler
+		char harf='b'; // karakter tutan değişken
+		byte sayiByte = 127; 
+		short sayiShort= 128;
+		int sayiInt = 33234;
+		long sayiLong = 3213123;
+		float sayiFloat = 12;
+		double sayiDouble = 4.1232;
+		
+		System.out.println(harf +"--" + sayiByte +"--" + sayiShort +"--" + sayiInt +"--" + sayiLong +"--" + sayiFloat +"--" + sayiDouble );
+		
+	}
+
+}
