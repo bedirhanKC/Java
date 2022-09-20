e91f1742d6970f2ffe09fe766a0b038254c557da
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-09-20 18:53:35 +0300

    Add files via upload

---
+++ Ogrenim_Kayitlari/ders8_do_while.java
@@ -0,0 +1,24 @@
+package intro;
+
+public class ders8_do_while {
+
+	public static void main(String[] args) {
+
+		int j=100;
+		
+		do {
+		
+			System.out.println(j);
+			
+		}while(j<10); //do noktasından döngü bir kere çalışır ve parantezin içi sağlamaya devam ettiği sürece döngü devam eder
+		
+		System.out.println("Do while döngüsü sona erdi.");
+		
+	}
+
+}
+
+
+
+
+// int[] dizi
