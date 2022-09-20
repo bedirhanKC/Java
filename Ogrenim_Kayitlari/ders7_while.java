24934cad5292d0baed7e2848957564ccc10a37c4
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-09-20 18:53:22 +0300

    Add files via upload

---
+++ Ogrenim_Kayitlari/ders7_while.java
@@ -0,0 +1,20 @@
+package intro;
+
+public class ders7_while {
+
+	public static void main(String[] args) {
+
+		int i=0; 
+		
+		while(i<=10) { // parantezin içini sağladığı sürece döngü dönmeye devam eder
+			
+			System.out.println(i);
+			i++;
+			
+		}
+		
+		System.out.println("while döngüsü tamamlandi");
+		
+	}
+
+}
