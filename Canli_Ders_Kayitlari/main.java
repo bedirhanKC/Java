e48e53ec8abd8ddfc79e289ddfcfbda86894cce7
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-09-26 15:41:21 +0300

    Add files via upload

---
+++ Canli_Ders_Kayitlari/main.java
@@ -0,0 +1,33 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		int[] sayilar= {3,4,7,5,2,8};
+		
+		System.out.println("en buyuk sayi : " + EnBuyukSayiBulma(sayilar));
+		//Yazdirma(max);
+		
+	}
+	
+	public static int EnBuyukSayiBulma(int... sayilar) {
+		
+		int max=sayilar[0];
+		
+		for(int i=0 ; i<sayilar.length ; i++) {
+			
+			if(sayilar[i]>max) max=sayilar[i];
+			
+		}
+		
+		return max;
+		
+	}
+
+	public static void Yazdirma(int max) {
+		
+		System.out.println("En buyuk sayi "+max+"'dir");
+		
+	}
+	
+}
