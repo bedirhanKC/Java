2686b575eda713a41b812e4a3f6662b2864b744a
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-09-26 15:48:27 +0300

    Add files via upload

---
+++ ornekler/Main.java
@@ -0,0 +1,13 @@
+package ornek3_ders_13_classes_demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		DortIslem hesapMakinesi = new DortIslem();
+		
+		System.out.println(hesapMakinesi.Carp(5, 4));
+		
+	}
+
+}
