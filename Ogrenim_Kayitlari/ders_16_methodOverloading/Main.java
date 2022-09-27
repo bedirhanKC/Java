fdaa97270c3e8478b30ca06dea4551678de39856
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-09-27 18:11:12 +0300

    Add files via upload

---
+++ Ogrenim_Kayitlari/ders_16_methodOverloading/Main.java
@@ -0,0 +1,14 @@
+package ders_16_methodOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		DortIslem dortIslem = new DortIslem();
+		
+		System.out.println(dortIslem.topla(1, 2));
+		System.out.println(dortIslem.topla(1, 2, 3));
+
+	}
+
+}
